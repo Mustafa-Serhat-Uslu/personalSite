9a5b43c759cd8f7fa07b31777a976b4385631625
mustafa <mserhat.uslu@bombay.io>
2024-08-25 17:01:22 +0300

    add deploy.yml 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,4 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: 'https://<Mustafa-Serhat-Uslu>.github.io',
-  // base: 'personalSite',
 });
